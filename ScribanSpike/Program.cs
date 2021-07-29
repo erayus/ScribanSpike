d361bd017f6975f17cdeb871b15cec407e32b525
raymondt <raymondt@pageuppeople.com>
2021-07-29 11:32:43 +1000

    Initial commit

---
+++ ScribanSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScribanSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
