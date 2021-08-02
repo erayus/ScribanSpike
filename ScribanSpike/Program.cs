0d7d1545176cf810d31869e2b09b7da304c26cda
raymondt <raymondt@pageuppeople.com>
2021-08-02 16:19:49 +1000

    More complex use case

--- ScribanSpike/Program.cs
+++ ScribanSpike/Program.cs
@@ -4,45 +4,76 @@ using Stubble.Core.Builders;
 
 namespace ScribanSpike
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-	        var templateText = @"<table>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var templateText = @"
+	        <table>
 				<tr>
-					<th>Name</th>
-					<th>Level</th>
+					<th>{{TableName}}</th>
+					<th>{{TableLevel}}</th>
 				</tr>
-				{{ #Capabilities }}
+				{{ #CapabilityGroup.Capabilities }}
 					<tr>
 						<td>{{ CapabilityName }}</td>
 						<td>{{ RequirementLevel }}</td>
 					</tr>
-				{{ /Capabilities }}
+				{{ /CapabilityGroup.Capabilities }}
 			</table>
+		<hr/>
+		 <table>
+{{CapabilityGroupList}}
+						<tr>
+							<th>Name</th>
+							<th>Level</th>
+						</tr>
+						{{ #CapabilityGroup2.Capabilities }}
+							<tr>
+								<td>{{ CapabilityName }}</td>
+								<td>{{ RequirementLevel }}</td>
+							</tr>
+						{{ /CapabilityGroup2.Capabilities }}
+					</table>
 			";
-	        
-	        var localPd = new PD
-	        {
-		        CapabilityGroup = new CapabilityGroup
-		        {
-			        Capabilities = new List<Capability>
-			        {
-				        new Capability {CapabilityName = "NextGen1", RequirementLevel = "High"},
-				        new Capability {CapabilityName = "NextGen2", RequirementLevel = "Low"}
-			        }
-		        }
-	        };
-	        
-	        var stubble = new StubbleBuilder().Build();
-	        var result = stubble.Render(templateText, localPd.CapabilityGroup);
-	        
-            Console.WriteLine(result);
-        }
-    }
-    
-    public class PD {
+
+			{
+
+				var localPd = new PD
+				{
+					TableName = "Test Name",
+					TableLevel = "Test Level",
+					CapabilityGroup = new CapabilityGroup
+					{
+						Capabilities = new List<Capability>
+						{
+							new Capability {CapabilityName = "NextGen1", RequirementLevel = "High"},
+							new Capability {CapabilityName = "NextGen2", RequirementLevel = "Low"}
+						}
+					},
+					CapabilityGroup2 = new CapabilityGroup
+					{
+						Capabilities = new List<Capability>
+						{
+							new Capability {CapabilityName = "NextGen3", RequirementLevel = "High"},
+							new Capability {CapabilityName = "NextGen4", RequirementLevel = "Low"}
+						}
+					}
+				};
+
+				var stubble = new StubbleBuilder().Build();
+				var result = stubble.Render(templateText, localPd);
+
+				Console.WriteLine(result);
+			}
+		}
+	}
+
+	public class PD {
+		public string TableName { get; set; }
+		public string TableLevel { get; set; }
 		public CapabilityGroup CapabilityGroup { get; set; }
+		public CapabilityGroup CapabilityGroup2 { get; set; }
     }
 
     public class CapabilityGroup
